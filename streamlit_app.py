836f168e00048f5ad2a3b17b67b2e268ee72527f
richard.lopez <richard.lopez@et.bo>
2024-07-12 11:17:11 -0400

    utilizando el evento on_change en el textarea

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,13 +87,12 @@ st.header("Agregar un ticket")
 with st.container(): 
     issue = st.text_area("Describa el problema", key="issue", on_change=on_text_change)
     priority = st.selectbox("Prioridad", ["Alto", "Medio", "Bajo"], key="priority")
-    submitted = st.form_submit_button("Enviar")
+
 # We're adding tickets via an `st.form` and some input widgets. If widgets are used
 # in a form, the app will only rerun once the submit button is pressed.
-""" 
 with st.form("add_ticket_form"):
     submitted = st.form_submit_button("Enviar")
- """
+
 if submitted:
     # Make a dataframe for the new ticket and append it to the dataframe in session
     # state.
