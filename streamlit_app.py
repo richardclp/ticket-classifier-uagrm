ed02c3b20b0c492b9edff587518dec9b8515fa00
richard.lopez <richard.lopez@et.bo>
2024-07-15 10:15:42 -0400

    Cambio final 12-07-2024: - adicion de menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,10 @@ st.sidebar.markdown("""
 </style>
 <div >
     <ul class='sidebar-menu'>
-        <li><a href='#home'><span>HOME</span></a></li>
-        <li><a href='#create'><span>Crear Tickets</span></a></li>
+        <li><a href='#home'><span>Inicio</span></a></li>
+        <li><a href='#create'><span>Crear un Ticket</span></a></li>
         <li><a href='#view'><span>Ver Tickets</span></a></li>
-        <li><a href='#stats'><span>Estadisticas</span></a></li>
+        <li><a href='#stats'><span>Estadísticas</span></a></li>
     </ul>
 </div>
 """, unsafe_allow_html=True)
