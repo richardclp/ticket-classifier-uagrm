729e67ca6a3c2b7063faf6b531ee43b95c876e3b
richard.lopez <richard.lopez@et.bo>
2024-07-18 12:29:54 -0400

    animacion entre paginas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-# Parametros de título y descripción de la aplicación.
-st.set_page_config(page_title="TicketEase - TFG Maestria en Ciencia de Datos UAGRM)", page_icon="🎫")
+# Parametros de tÃ­tulo y descripciÃ³n de la aplicaciÃ³n.
+st.set_page_config(page_title="TicketEase - TFG Maestria en Ciencia de Datos UAGRM)", page_icon="ð«")
 
-# Renderizar el menú en la barra lateral izquierda
-st.sidebar.subheader("🎫 Contenido", divider='blue')
+# Renderizar el menÃº en la barra lateral izquierda
+st.sidebar.subheader("ð« Contenido", divider='blue')
 
-# Generar enlaces en la barra lateral para navegar a cada sección
+# Generar enlaces en la barra lateral para navegar a cada secciÃ³n
 st.sidebar.markdown("""
 <style>
 .sidebar-menu, .sidebar-menu li {
@@ -57,24 +57,24 @@ st.sidebar.markdown("""
 </style>
 <div >
     <ul class='sidebar-menu'>
-        <li><a href='#home'><span>🏠 Inicio</span></a></li>
-        <li><a href='#create'><span>📝 Crear un Ticket</span></a></li>
-        <li><a href='#view'><span>📋 Ver Tickets</span></a></li>
-        <li><a href='#stats'><span>📊 Estadísticas</span></a></li>
+        <li><a href='#home'><span>ð  Inicio</span></a></li>
+        <li><a href='#create'><span>ð Crear un Ticket</span></a></li>
+        <li><a href='#view'><span>ð Ver Tickets</span></a></li>
+        <li><a href='#stats'><span>ð EstadÃ­sticas</span></a></li>
     </ul>
 </div>
 """, unsafe_allow_html=True)
 st.markdown(
 """<br id="home"/><hr style="margin:0;"/>
-<h1 style="padding-top: 0;">🎫 <em>TicketEase</em></h1>
-<h4>Descripción de la Aplicación de <strong>TicketEase</strong> <em>(Gestión de Tickets)</em></h4>
-<p>En esta aplicación estamos implementando un flujo de trabajo de tickets de soporte. El usuario puede crear un ticket, editar tickets existentes y ver algunas estadísticas.</p>
+<h1 style="padding-top: 0;">ð« <em>TicketEase</em></h1>
+<h4>DescripciÃ³n de la AplicaciÃ³n de <strong>TicketEase</strong> <em>(GestiÃ³n de Tickets)</em></h4>
+<p>En esta aplicaciÃ³n estamos implementando un flujo de trabajo de tickets de soporte. El usuario puede crear un ticket, editar tickets existentes y ver algunas estadÃ­sticas.</p>
 
-<h5>Características principales</h5>
+<h5>CaracterÃ­sticas principales</h5>
 <ol>
     <li><strong>Crear Tickets:</strong> Los usuarios pueden describir el problema y asignar una prioridad al ticket antes de enviarlo.</li>
-    <li><strong>Editar Tickets:</strong> Los tickets existentes pueden ser modificados para actualizar la información relevante o cambiar el estado.</li>
-    <li><strong>Ver Estadísticas:</strong> La aplicación proporciona gráficos que muestran estadísticas sobre el estado y la prioridad de los tickets, permitiendo una visión rápida y eficiente del flujo de trabajo de soporte.</li>
+    <li><strong>Editar Tickets:</strong> Los tickets existentes pueden ser modificados para actualizar la informaciÃ³n relevante o cambiar el estado.</li>
+    <li><strong>Ver EstadÃ­sticas:</strong> La aplicaciÃ³n proporciona grÃ¡ficos que muestran estadÃ­sticas sobre el estado y la prioridad de los tickets, permitiendo una visiÃ³n rÃ¡pida y eficiente del flujo de trabajo de soporte.</li>
 </ol>
 """, unsafe_allow_html=True
 )
@@ -88,25 +88,25 @@ if "df" not in st.session_state:
     # Realizando algunas descripciones de problemas ficticios.
     issue_descriptions = [
         "Problemas de conectividad de red en la oficina",
-        "Aplicación de software se bloquea al iniciar",
-        "La impresora no responde a los comandos de impresión",
-        "Tiempo de inactividad del servidor de correo electrónico",
+        "AplicaciÃ³n de software se bloquea al iniciar",
+        "La impresora no responde a los comandos de impresiÃ³n",
+        "Tiempo de inactividad del servidor de correo electrÃ³nico",
         "Fallo en la copia de seguridad de datos",
-        "Problemas de autenticación de inicio de sesión",
-        "Degradación del rendimiento del sitio web",
+        "Problemas de autenticaciÃ³n de inicio de sesiÃ³n",
+        "DegradaciÃ³n del rendimiento del sitio web",
         "Vulnerabilidad de seguridad identificada",
         "Fallo de hardware en la sala de servidores",
         "Empleado no puede acceder a los archivos compartidos",
-        "Fallo de conexión a la base de datos",
-        "La aplicación móvil no sincroniza datos",
-        "Problemas con el sistema de teléfonos VoIP",
-        "Problemas de conexión VPN para empleados remotos",
+        "Fallo de conexiÃ³n a la base de datos",
+        "La aplicaciÃ³n mÃ³vil no sincroniza datos",
+        "Problemas con el sistema de telÃ©fonos VoIP",
+        "Problemas de conexiÃ³n VPN para empleados remotos",
         "Actualizaciones del sistema causan problemas de compatibilidad",
-        "El servidor de archivos se está quedando sin espacio de almacenamiento",
-        "Alertas del sistema de detección de intrusos",
-        "Errores en el sistema de gestión de inventarios",
+        "El servidor de archivos se estÃ¡ quedando sin espacio de almacenamiento",
+        "Alertas del sistema de detecciÃ³n de intrusos",
+        "Errores en el sistema de gestiÃ³n de inventarios",
         "Los datos de clientes no se cargan en el CRM",
-        "La herramienta de colaboración no envía notificaciones",
+        "La herramienta de colaboraciÃ³n no envÃ­a notificaciones",
     ]
 
     # Genere el dataframe de datos con 100 filas o tickets.
@@ -123,7 +123,7 @@ if "df" not in st.session_state:
     df = pd.DataFrame(data)
 
     # Guardando el dataframe de datos en la session_state (un objeto similar a un diccionario que persiste a lo largo
-    # de las ejecuciones de la página). Esto garantiza que nuestros datos persistan cuando se actualiza la aplicación.
+    # de las ejecuciones de la pÃ¡gina). Esto garantiza que nuestros datos persistan cuando se actualiza la aplicaciÃ³n.
     st.session_state.df = df
 
 # Inicializa los valores en session_state si no existen
@@ -133,11 +133,11 @@ if 'priority' not in st.session_state:
     st.session_state.priority = "Medio"
 
 def remove_accents(text):
-    """Elimina acentos y otras marcas diacríticas de un texto determinado."""
+    """Elimina acentos y otras marcas diacrÃ­ticas de un texto determinado."""
     normalized = unicodedata.normalize('NFD', text)
     return ''.join(char for char in normalized if unicodedata.category(char) != 'Mn')
 
-# Definir la función de callback para actualizar la prioridad
+# Definir la funciÃ³n de callback para actualizar la prioridad
 def on_text_change():
     issue_text = remove_accents(st.session_state.issue.lower())
     
@@ -151,7 +151,7 @@ def on_text_change():
     priority_medium = [
         "importante", "moderado", "media prioridad", 
         "prioridad media", "significativo", "necesario", 
-        "esencial", "considerable", "atención media", "atencion media", "relevante"
+        "esencial", "considerable", "atenciÃ³n media", "atencion media", "relevante"
     ]
 
     priority_low = [
@@ -160,7 +160,7 @@ def on_text_change():
         "retrasable", "deferible", "poco urgente", "secundario"
     ]
     
-    # Define una función auxiliar para verificar si alguna palabra clave está en el texto
+    # Define una funciÃ³n auxiliar para verificar si alguna palabra clave estÃ¡ en el texto
     def contains_keyword(text, keywords):
         return any(keyword in text for keyword in keywords)
     
@@ -198,20 +198,20 @@ if submitted:
         ]
     )
 
-    # Mostrando un pequeño mensaje de éxito.
-    st.write("Ticket guardado! Aquí están los detalles del ticket.:")
+    # Mostrando un pequeÃ±o mensaje de Ã©xito.
+    st.write("Ticket guardado! AquÃ­ estÃ¡n los detalles del ticket.:")
     st.dataframe(df_new, use_container_width=True, hide_index=True)
     st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
 
 st.markdown("""<br id="view"/><hr/>""", unsafe_allow_html=True)
-# Mostrar sección para ver y editar tickets existentes en una tabla.
+# Mostrar secciÃ³n para ver y editar tickets existentes en una tabla.
 st.subheader("Tickets existentes", divider='blue')
 st.write(f"Numero de tickets: `{len(st.session_state.df)}`")
 
 st.info(
-    "Puedes editar los tickets haciendo doble clic en una celda. ¡Observe cómo los gráficos a continuación se actualizan"
-    "automáticamente! También puede ordenar la tabla haciendo clic en los encabezados de las columnas.",
-    icon="✍️",
+    "Puedes editar los tickets haciendo doble clic en una celda. Â¡Observe cÃ³mo los grÃ¡ficos a continuaciÃ³n se actualizan"
+    "automÃ¡ticamente! TambiÃ©n puede ordenar la tabla haciendo clic en los encabezados de las columnas.",
+    icon="âïž",
 )
 
 # Mostrando el dataframe de los tickets con `st.data_editor`. Esto permite al usuario editar 
@@ -234,25 +234,25 @@ edited_df = st.data_editor(
             required=True,
         ),
     },
-    # Deshabilitando la edición de las columnas ID y Fecha Enviado.
+    # Deshabilitando la ediciÃ³n de las columnas ID y Fecha Enviado.
     disabled=["ID", "Fecha Enviado"],
 )
 
 st.markdown("""<br id="stats"/><hr/>""", unsafe_allow_html=True)
-# Muestra algunas métricas y gráficos de los tickets.
+# Muestra algunas mÃ©tricas y grÃ¡ficos de los tickets.
 st.subheader("Estadisticas", divider='blue')
 
-# Mostrando métricas una al lado de la otra usando `st.columns` y `st.metric`.
+# Mostrando mÃ©tricas una al lado de la otra usando `st.columns` y `st.metric`.
 col1, col2, col3 = st.columns(3)
 num_open_tickets = len(st.session_state.df[st.session_state.df.Estado == "Abierto"])
-col1.metric(label="Número de tickets abiertos", value=num_open_tickets, delta=10)
+col1.metric(label="NÃºmero de tickets abiertos", value=num_open_tickets, delta=10)
 col2.metric(label="Tiempo de primera respuesta (horas)", value=5.2, delta=-1.5)
-col3.metric(label="Tiempo medio de resolución (horas)", value=16, delta=2)
+col3.metric(label="Tiempo medio de resoluciÃ³n (horas)", value=16, delta=2)
 
-# Mostrar dos gráficos usando `st.altair_chart`.
+# Mostrar dos grÃ¡ficos usando `st.altair_chart`.
 st.write("")
 st.write("##### Estado del ticket por mes")
-# creando un gráfico de barras apiladas, con la cantidad de tickets por estado para cada mes
+# creando un grÃ¡fico de barras apiladas, con la cantidad de tickets por estado para cada mes
 status_plot = (
     alt.Chart(edited_df)
     .mark_bar()
@@ -274,7 +274,7 @@ priority_counts = edited_df['Prioridad'].value_counts().reset_index()
 priority_counts.columns = ['Prioridad', 'count']
 priority_counts['percentage'] = (priority_counts['count'] / priority_counts['count'].sum()) * 100
 
-# Creando un gráfico de pastel con las etiquetas de porcentaje de tickets por estado
+# Creando un grÃ¡fico de pastel con las etiquetas de porcentaje de tickets por estado
 priority_plot = (
     alt.Chart(priority_counts)
     .mark_arc()
@@ -291,38 +291,12 @@ priority_plot = (
 )
 st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
 
-# Cargar el código JavaScript para smooth scrolling
+# Cargar el cÃ³digo JavaScript para smooth scrolling
 st.components.v1.html("""
+ <script src="https://cdn.jsdelivr.net/npm/smoothscroll-polyfill@0.4.3/dist/smoothscroll.min.js"></script>
 <script>
-    window.onload = function() {
-        // Encuentra todos los enlaces que apuntan a secciones internas con el atributo href que comienza con #
-        const links = document.querySelectorAll('a[href^="#"]');
-        console.log("links:", links);
-        // Itera sobre cada enlace
-        links.forEach(link => {
-            // Agrega un evento de clic a cada enlace
-            link.addEventListener("click", function(e) {
-                // Previene el comportamiento predeterminado del enlace
-                e.preventDefault();
-                console.log("link:", link);
-                
-                // Obtiene el destino del enlace (la sección a la que se dirige)
-                const targetId = this.getAttribute("href").substring(1);
-                const targetElement = document.getElementById(targetId);
-                
-                // Verifica si el elemento destino existe
-                if (targetElement) {
-                    // Calcula la posición del elemento en la página
-                    const offsetTop = targetElement.offsetTop;
-                    
-                    // Desplaza la página suavemente hasta la posición del elemento
-                    window.scrollTo({
-                        top: offsetTop,
-                        behavior: "smooth"
-                    });
-                }
-            });
-        });
-    };
+    document.addEventListener("DOMContentLoaded", function() {
+        smoothscroll.polyfill();
+    });
 </script>
 """, height=0)
